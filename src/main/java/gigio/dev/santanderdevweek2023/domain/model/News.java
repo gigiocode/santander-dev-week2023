438da67fdda79468e800a58dbf55db030da61e90
Giovane Guimarães <giovane.meca@gmail.com>
2023-10-21 13:43:45 -0300

    Gerando resources YML

--- src/main/java/gigio/dev/santanderdevweek2023/domain/model/News.java
+++ src/main/java/gigio/dev/santanderdevweek2023/domain/model/News.java
@@ -1,5 +1,8 @@
 package gigio.dev.santanderdevweek2023.domain.model;
 
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
 public class News extends BaseItem {
 
 }
---
+++ src/main/java/gigio/dev/santanderdevweek2023/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package gigio.dev.santanderdevweek2023.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import gigio.dev.santanderdevweek2023.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
