407a15513a6a65bf2eab84aa1a14ecbfa4d14706
Giovane Guimarães <giovane.meca@gmail.com>
2023-10-21 15:13:59 -0300

    Bugfix CORS

--- src/main/java/gigio/dev/santanderdevweek2023/SantanderDevWeek2023Application.java
+++ src/main/java/gigio/dev/santanderdevweek2023/SantanderDevWeek2023Application.java
@@ -3,10 +3,10 @@ package gigio.dev.santanderdevweek2023;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-// import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
-// @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class SantanderDevWeek2023Application {
 
